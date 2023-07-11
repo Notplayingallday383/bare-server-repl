750b1a131e3b845f85c9260c12d7099048f5fe5e
John Glynn <playingallday383@gmail.com>
2023-07-11 10:39:33 -0400

    Add files

---
+++ server.sh
@@ -0,0 +1,5 @@
+echo Starting Bare Server
+npx bare-server-node
+echo NPX Initialized
+npx bare-server-node --port 8080 --host localhost
+echo Server is running on: PORT:8080 on REPL.LOCALHOST or visit your repl and see the changes
